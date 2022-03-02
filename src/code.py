e47604ce1b652194bcfd12683ce70d39fe811319
jeremypatrickdahan <jeremydahan@protonmail.com>
2022-03-02 21:13:54 +0100

    Update to auto detect sensors on start

--- src/code.py
+++ src/code.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
-#
-# SPDX-License-Identifier: Unlicense
 import time
 from adafruit_magtag.magtag import MagTag
 from adafruit_magtag.graphics import Graphics
@@ -8,14 +5,10 @@ from adafruit_progressbar.progressbar import HorizontalProgressBar
 import board
 import alarm
 
+import sensors
 
-import adafruit_scd4x
-import adafruit_ccs811
-from adafruit_pm25.i2c import PM25_I2C
-from mics6814 import MICS6814 
-import busio
 
-i2c = busio.I2C(board.SCL, board.SDA)
+sensors.init_connected_sensors()
 
 magtag = MagTag(rotation=0)
 
@@ -24,35 +17,17 @@ H = magtag.graphics.display.height
 M = 3 # Margin
 
 SLEEP = True
-use_pm25 = True
-use_MICS6814 = True
-use_SCD41 = True
-BIG_PPM = True
 
 
-if(use_pm25):
-    pm25_reset_pin = None
-    pm25 = PM25_I2C(i2c, pm25_reset_pin)
-    aqdata = None
-    while aqdata is None:
-        try:
-            aqdata = pm25.read()
-            # print(aqdata)
-        except RuntimeError:
-            print("Unable to read from sensor, retrying...")
-            time.sleep(1)
+print(sensors.enabled_sensors)
 
-if(use_SCD41):
-    scd4x = adafruit_scd4x.SCD4X(i2c)
-    scd4x.start_periodic_measurement()
-    while scd4x.CO2 is None :
-        time.sleep(0.1)
-
-if use_MICS6814:
-    mics6814 = MICS6814(i2c)
-    mics6814.set_led(0,0,0)
-    mics6814.set_brightness(0)
-
+def get_battery_progress():
+    progress = 100*(magtag.peripherals.battery-3.6)/(4.1-3.6)
+    if(progress > 100):
+        progress = 100
+    elif (progress < 0):
+        progress = 0
+    return progress
 
 
 # # Create a new progress_bar object at (x, y)
@@ -64,43 +39,112 @@ progress_bar = HorizontalProgressBar(
     bar_color=0x777777,
 )
 magtag.graphics.splash.append(progress_bar)
-progress_bar.progress = 100*(magtag.peripherals.battery-3.6)/(4.23-3.6)
+progress_bar.progress = get_battery_progress()
 
-magtag.add_text( ## "CO2"
-        text_position=(M,M,),
-        text_scale=1,
-        text_font="fonts/Arial-Bold-12.pcf",
-        text_anchor_point=(0, 0),  # top left,
-)
-magtag.set_text("CO2", 0, auto_refresh=False)
+texts = dict()
+index = 0
+current_position = 0
+small_font_height = 13
+big_font_height = 44
 
-magtag.add_text( ## "ppm"
-        text_position=(W - M, M,),
-        text_scale=1,
-        text_font="fonts/Arial-Bold-12.pcf",
-        text_anchor_point=(1, 0),  # top right,
-)
-magtag.set_text("ppm", 1, auto_refresh=False)
+def add_text(name, text_position, text_scale, text_font, text_anchor_point):
+    global index
+    global texts
+    texts[name] = index
+    index+= 1
+    magtag.add_text(
+            text_position=text_position,
+            text_scale=text_scale,
+            text_font=text_font,
+            text_anchor_point=text_anchor_point
+    )
 
-magtag.add_text( ## CO2 value
-        text_position=(W - M,16,),
-        text_scale=3,
-        text_font="fonts/Arial-Bold-12.pcf",
-        text_anchor_point=(1, 0),  # top right
-)
+def set_text(name, text, auto_refresh=False):
+    magtag.set_text(text, texts[name], auto_refresh)
+    
 
-magtag.add_text( ## Temp
-        text_position=(W - M,60,),
-        text_scale=3,
-        text_font="fonts/Arial-Bold-12.pcf",
-        text_anchor_point=(1, 0),  # top left
-)
-magtag.add_text( ## humidity
-        text_position=(W - M,105,),
-        text_scale=3,
-        text_font="fonts/Arial-Bold-12.pcf",
-        text_anchor_point=(1, 0),  # top left
-)
+current_position = M
+
+if(sensors.enabled_sensors['SCD41']):
+    add_text( 
+            "CO2_title",
+            text_position=(M,current_position,),
+            text_scale=1,
+            text_font="fonts/Arial-Bold-12.pcf",
+            text_anchor_point=(0, 0),  # top left,
+    )
+    set_text(name = "CO2_title", text = "CO2", auto_refresh=False)
+
+    add_text( 
+            "ppm_title",
+            text_position=(W - M, current_position,),
+            text_scale=1,
+            text_font="fonts/Arial-Bold-12.pcf",
+            text_anchor_point=(1, 0),  # top right,
+    )
+    set_text(name = "ppm_title", text="ppm", auto_refresh=False)
+
+    current_position += small_font_height
+
+    add_text(
+            "CO2_value",
+            text_position=(W - M,current_position,),
+            text_scale=3,
+            text_font="fonts/Arial-Bold-12.pcf",
+            text_anchor_point=(1, 0),  # top right
+    )
+
+    current_position += big_font_height
+
+    add_text(
+        "Temp_value",
+            text_position=(W - M, current_position,),
+            text_scale=3,
+            text_font="fonts/Arial-Bold-12.pcf",
+            text_anchor_point=(1, 0),  # top left
+    )
+
+    current_position += big_font_height
+
+    add_text(
+        "humidity_value",
+            text_position=(W - M,current_position,),
+            text_scale=3,
+            text_font="fonts/Arial-Bold-12.pcf",
+            text_anchor_point=(1, 0),  # top left
+    )
+
+    current_position += big_font_height
+
+if(sensors.enabled_sensors['CCS811']):
+    add_text( 
+            "VOC_title",
+            text_position=(M,current_position,),
+            text_scale=1,
+            text_font="fonts/Arial-Bold-12.pcf",
+            text_anchor_point=(0, 0),  # top left,
+    )
+    set_text(name = "VOC_title", text="VOC", auto_refresh=False)
+
+    add_text(  
+            "ppb_title",
+            text_position=(W - M, current_position,),
+            text_scale=1,
+            text_font="fonts/Arial-Bold-12.pcf",
+            text_anchor_point=(1, 0),  # top right,
+    )
+    set_text(name = "ppb_title", text="pbb", auto_refresh=False)
+
+    current_position += small_font_height
+
+
+    add_text( 
+            "VOC_value",
+            text_position=(W - M,current_position,),
+            text_scale=3,
+            text_font="fonts/Arial-Bold-12.pcf",
+            text_anchor_point=(1, 0),  # top right
+    )
 
 
 
@@ -160,13 +204,17 @@ a_alarm = alarm.pin.PinAlarm(pin=board.BUTTON_A, value=False, pull=True) #note p
 while True:
     if(magtag.peripherals.button_b_pressed):
         SLEEP = not SLEEP
-    magtag.set_text("%d" % scd4x.CO2, 2, auto_refresh=False)
-    magtag.set_text("%0.f°C" % scd4x.temperature, 3, auto_refresh=False)
-    magtag.set_text("%0.f%%" % scd4x.relative_humidity, 4, auto_refresh=False)
-    progress_bar.progress = 100*(magtag.peripherals.battery-3.6)/(4.23-3.6)
+    sensors.refresh_readings()
+    if(sensors.enabled_sensors['SCD41']):
+        set_text(name = "CO2_value", text= "%d" % sensors.readings['CO2'], auto_refresh=False)
+        set_text(name = "Temp_value", text= "%0.f°C" % sensors.readings['temperature'], auto_refresh=False)
+        set_text(name = "humidity_value", text= "%0.f%%" % sensors.readings['relative_humidity'], auto_refresh=False)
+    if(sensors.enabled_sensors['CCS811']):
+        set_text(name = "VOC_value", text= "%d" % sensors.readings['TVOC'], auto_refresh=False)
+    progress_bar.progress = get_battery_progress()
     magtag.refresh()
     if(SLEEP):
-        scd4x.stop_periodic_measurement()
+        sensors.put_sensors_to_sleep()
         time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 600) # 600 seconds = 5 minutes
         alarm.exit_and_deep_sleep_until_alarms(time_alarm, a_alarm)
     else:
---
+++ src/sensors.py
@@ -0,0 +1,104 @@
+import board
+import alarm
+import time
+
+import busio
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+pm25 = None
+scd4x = None
+mics6814 = None
+ccs811 = None
+
+CCS811_LOW_POWER_MODE = None
+
+def init_sensor(sensor):
+	global pm25
+	global scd4x
+	global mics6814
+	global ccs811
+	global CCS811_LOW_POWER_MODE
+	if(sensor == 'PM25'):
+	    from adafruit_pm25.i2c import PM25_I2C
+	    pm25_reset_pin = None
+	    pm25 = PM25_I2C(i2c, pm25_reset_pin)
+	    while aqdata is None:
+	        try:
+	            pm25.read()
+	        except RuntimeError:
+	            time.sleep(0.1)
+	elif(sensor == 'SCD41'):
+	    import adafruit_scd4x
+	    scd4x = adafruit_scd4x.SCD4X(i2c)
+	    scd4x.start_periodic_measurement()
+	    while scd4x.CO2 is None :
+	        time.sleep(0.1)
+	elif(sensor == 'MICS6814'):
+	    from mics6814 import MICS6814 
+	    mics6814 = MICS6814(i2c)
+	    # mics6814.set_led(0,0,0)
+	    # mics6814.set_brightness(0)
+	elif(sensor == 'CCS811'):
+	    import adafruit_ccs811
+	    ccs811 = adafruit_ccs811.CCS811(i2c)
+	    CCS811_LOW_POWER_MODE = adafruit_ccs811.DRIVE_MODE_60SEC
+	    # Wait for the sensor to be ready
+	    while not ccs811.data_ready:
+	        time.sleep(0.1)
+
+readings = dict()
+
+def refresh_readings():
+	global pm25
+	global scd4x
+	global mics6814
+	global ccs811
+	# if(enabled_sensors['PM25']):
+		# init_sensor('PM25')
+	if(enabled_sensors['SCD41']):
+		readings['CO2'] = scd4x.CO2
+		readings['temperature'] = scd4x.temperature
+		readings['relative_humidity'] = scd4x.relative_humidity
+	# if enabled_sensors['MICS6814']:
+	if enabled_sensors['CCS811']:
+		readings['TVOC'] = ccs811.tvoc
+		readings['eCO2'] = ccs811.eco2
+	return readings
+
+
+def put_sensors_to_sleep():
+	global pm25
+	global scd4x
+	global mics6814
+	global ccs811
+	if(enabled_sensors['SCD41']):
+		scd4x.stop_periodic_measurement()
+	if(enabled_sensors['CCS811']):
+		ccs811.drive_mode = CCS811_LOW_POWER_MODE
+
+
+while not i2c.try_lock():
+    time.sleep(0.1)
+
+addresses = i2c.scan()
+i2c.unlock()
+
+print(addresses)
+
+enabled_sensors = dict(
+    PM25 = 0x12 in addresses,
+    SCD41 = 0x62 in addresses,
+    MICS6814 = 0x19 in addresses and False,
+    CCS811 = 0x5a in addresses,
+)
+
+def init_connected_sensors():
+	if(enabled_sensors['PM25']):
+		init_sensor('PM25')
+	if(enabled_sensors['SCD41']):
+		init_sensor('SCD41')
+	if enabled_sensors['MICS6814']:
+		init_sensor('MICS6814')
+	if enabled_sensors['CCS811']:
+		init_sensor('CCS811')
